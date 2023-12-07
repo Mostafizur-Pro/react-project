28251e9ad67cbb5cf3c7cff7657d8c89986a187e
mostafizur-pro <mostafizur0195@gmail.com>
2023-12-07 11:29:18 +0600

    footer and navbar add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 
 function App() {
   return (
     <div className="max-w-screen-xl mx-auto">
-     <RouterProvider router={router}></RouterProvider>
-     
+      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
